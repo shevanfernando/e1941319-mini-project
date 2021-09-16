35207aff06154e970b820d166ecdc7e4f01e4fb9
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-09-16 20:46:10 +0530

    implement login

---
+++ app/src/main/java/com/example/e1941319_mini_project/Login.java
@@ -0,0 +1,13 @@
+package com.example.e1941319_mini_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
