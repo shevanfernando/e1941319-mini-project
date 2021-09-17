3d3b2c681e40428ab2e73636ac4dc558f220c7ce
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-09-17 11:30:34 +0530

    implement login

--- app/src/main/java/com/example/e1941319_mini_project/Login.java
+++ app/src/main/java/com/example/e1941319_mini_project/Login.java
@@ -1,7 +1,9 @@
 package com.example.e1941319_mini_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+    public void onLoginClick(View view) {
+
     }
 }
