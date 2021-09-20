d8e3b8387b1e24454747da1a2d8ba6c50fced645
Shevan Fernando <81354288+shevanfernando@users.noreply.github.com>
2021-09-20 17:11:27 +0530

    implement add package

--- app/src/main/java/com/example/e1941319_mini_project/AddPackageActivity.java
+++ app/src/main/java/com/example/e1941319_mini_project/AddPackageActivity.java
@@ -1,9 +1,15 @@
 package com.example.e1941319_mini_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -16,6 +22,7 @@ import android.widget.Toast;
 import com.example.e1941319_mini_project.dto.PackageDTO;
 
 import java.util.List;
+import java.util.Objects;
 
 public class AddPackageActivity extends AppCompatActivity {
 
@@ -23,6 +30,7 @@ public class AddPackageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_package);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         DBAdapter db = new DBAdapter();
 
@@ -33,9 +41,7 @@ public class AddPackageActivity extends AppCompatActivity {
         AutoCompleteTextView txt_search_customer = (AutoCompleteTextView) findViewById(R.id.txt_search_customer);
 
         db.getAllCustomers().observe(AddPackageActivity.this, res -> {
-            if (res.isEmpty()) {
-                Toast.makeText(AddPackageActivity.this, "Customers ID's Fetch Failed.", Toast.LENGTH_SHORT).show();
-            } else {
+            if (!res.isEmpty()) {
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(AddPackageActivity.this, android.R.layout.simple_list_item_1, res);
                 txt_search_customer.setAdapter(adapter);
             }
@@ -54,5 +60,34 @@ public class AddPackageActivity extends AppCompatActivity {
                 }
             });
         });
+    }
+
+
+    // add menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    // handle the menu item actions
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itm_logout: {
+                Toast.makeText(AddPackageActivity.this, "Logout...", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(AddPackageActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case android.R.id.home: {
+                Toast.makeText(AddPackageActivity.this, "Redirect to Staff Dashboard.", Toast.LENGTH_SHORT).show();
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
